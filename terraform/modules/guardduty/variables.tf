d649cc5ff0603f35677fc1344f76d2db5ae3f6d3
Fausto <faustrosado@gmail.com>
2025-08-29 09:37:08 -0400

    Add only p2w12-quantum-shield directory content

--- terraform/modules/guardduty/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "s3_bucket_name" {
-  description = "S3 bucket name for threat intelligence"
-  type        = string
-}
--- terraform/modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "threat_intel_bucket_name" {
-  description = "Name of the threat intelligence S3 bucket"
-  value       = aws_s3_bucket.threat_intel.bucket
-}
-
-output "threat_intel_bucket_arn" {
-  description = "ARN of the threat intelligence S3 bucket"
-  value       = aws_s3_bucket.threat_intel.arn
-}
