b9259095b46c4da4771ae759a7810878fe3cdafc
[Team Member] <team.member@example.com>
2025-08-29 09:37:08 -0400

    Add only p2w12-quantum-shield directory content

--- terraform/modules/security_hub/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "security_hub_account_id" {
-  description = "Security Hub account ID"
-  value       = aws_securityhub_account.main.id
-}
-
-output "security_hub_arn" {
-  description = "Security Hub ARN"
-  value       = aws_securityhub_account.main.arn
-}
